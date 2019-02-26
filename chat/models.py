6be04a6252c470ea192f46d5c2658deffcd2c19f
Juan Mejia <jgmejiah@dane.gov.co>
2019-02-26 14:15:45 -0500

    Support for sending MAX_MESSAGES to chosen room

--- chat/models.py
+++ chat/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=1024)
+    room = models.CharField(max_length=50)
     timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
