2cf7116dfbbe5e8bf1d42bbd85e578fc9c0faae0
Juan Mejia <jgmejiah@dane.gov.co>
2019-02-25 21:17:49 -0500

    Pass request.user to the view

--- chat/views.py
+++ chat/views.py
@@ -1,12 +1,18 @@
 # chat/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 
+
+
 def index(request):
     return render(request, 'index.html', {})
 
+@login_required
 def room(request, room_name):
+    print('Request: =========> %s' %request.user)
     return render(request, 'room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'user': request.user
     })
