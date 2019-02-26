f58c5d9f18cf3e2a1177da91534735294bead00f
Juan Mejia <jgmejiah@dane.gov.co>
2019-02-25 21:16:48 -0500

    Message model creation

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=1024)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return ('%s sent the message %s at %s' %(self.user, self.message, self.timestamp))
