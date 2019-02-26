46927807fd449900ff969b70e5beadc2a05fe107
Juan Mejia <jgmejiah@dane.gov.co>
2019-02-26 17:08:21 -0500

    TESTS: not completed, not worknig

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+import pytest
+from channels.testing import WebsocketCommunicator
+from chat.consumers import ChatConsumer
+from channels.routing import URLRouter
+from django.conf.urls import url
 
-# Create your tests here.
+
+@pytest.mark.asyncio
+@pytest.mark.django_db
+async def test_consumer():
+    application = URLRouter([url(r'^ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer),])
+#    communicator = WebsocketCommunicator(ChatConsumer, '/chat/test1', {'url_route': '/chat/test1/'})
+    communicator = WebsocketCommunicator(application, 'ws/chat/test1/')
+    await communicator.connect()
+#    connected, subprotocol = await communicator.connect()
+#    assert connected
+    await communicator.send_input({
+        'type': 'chat_message',
+        'message': 'Hola mundo!',
+        'timestamp': '12:12:12',
+        'user': 'test_user'
+    })
+    event = await communicator.receive_output(timeout=1)
+    print ('%s'%event)
+    assert 'Chat room' in event
